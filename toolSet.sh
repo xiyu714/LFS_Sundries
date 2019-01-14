5d62329d541ce890cde5a5d49480011353ba8f32
xiyu714 <35284881+xiyu714@users.noreply.github.com>
2019-01-14 15:50:55 +0800

    1

--- toolSet.sh
+++ toolSet.sh
@@ -1,7 +1,7 @@
 ## 安装必要的工具
 
-apt install build-essential
-apt install bison gawk texinfo
+apt install -y build-essential
+apt install -y bison gawk texinfo
 
 ln -svf /usr/bin/yacc /usr/bin/bison
 ln -svf /usr/bin/awk /usr/bin/gawk
